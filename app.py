d92bbf0ebc157f571c270029cc57c279364d7421
hruthik-harish6 <hruthikharish6@gmail.com>
2025-11-02 12:20:48 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -397,4 +397,6 @@ def export_excel():
     return send_file(output, as_attachment=True, download_name="cashflow_data.xlsx")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0",port=port)
+
